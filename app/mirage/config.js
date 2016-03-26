c697ff327920c8bee619724763afcf6539f63fb2
acareaga <aaroncareaga@gmail.com>
2016-03-26 14:19:11 -0600

    Added mirage to mock endpoints until db and server are wired

---
+++ app/mirage/config.js
@@ -0,0 +1,40 @@
+export default function() {
+  this.get('/mountains', function() {
+    return {
+      data: [{
+        type: 'mountains',
+        id: 1,
+        attributes: {
+          title: 'Grand Old Mansion',
+          owner: 'Veruca Salt',
+          city: 'San Francisco',
+          type: 'Estate',
+          bedrooms: 15,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+        }
+      }, {
+        type: 'mountains',
+        id: 2,
+        attributes: {
+          title: 'Urban Living',
+          owner: 'Mike Teavee',
+          city: 'Seattle',
+          type: 'Condo',
+          bedrooms: 1,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+        }
+      }, {
+        type: 'mountains',
+        id: 3,
+        attributes: {
+          title: 'Downtown Charm',
+          owner: 'Violet Beauregarde',
+          city: 'Portland',
+          type: 'Apartment',
+          bedrooms: 3,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+        }
+      }]
+    };
+  });
+}
