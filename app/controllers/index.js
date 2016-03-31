a90829e757eea36e4f1b29157f3cfa0e4001dabc
acareaga <aaroncareaga@gmail.com>
2016-03-31 09:01:20 -0600

    Beta form on index working. Need to add model to store emails

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  emailAddress: '',
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
   filteredList: null,
   actions: {
+    saveInvitation() {
+       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+       this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+       this.set('emailAddress', '');
+     },
     autoComplete(param) {
       if(param !== "") {
         this.store.query('mountain', {city: param}).then((result) => {
